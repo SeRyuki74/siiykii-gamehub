a0322e40b1026b74a14ac5d319d05d0a3b163e6f
SeRyuki74 <ariqmulya01@gmail.com>
2026-06-02 18:34:38 +0700

    New

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+document.querySelectorAll('.card').forEach(card=>{
+card.addEventListener('mousemove',(e)=>{
+card.style.transform='translateY(-10px) scale(1.03)';
+});
+
+card.addEventListener('mouseleave',()=>{
+card.style.transform='translateY(0px)';
+});
+});
